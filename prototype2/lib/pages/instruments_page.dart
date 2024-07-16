e789cb8c57a2d6145a8be8c5bfea0046b44c9e79
Slozzyondul <solomonondula@gmail.com>
2024-07-16 12:47:57 +0300

    adding containers to store images in an orderly manner

--- prototype2/lib/pages/instruments_page.dart
+++ prototype2/lib/pages/instruments_page.dart
@@ -56,35 +56,64 @@ class _AllInstrumentsPage extends State<AllInstrumentsPage> {
   }
 
   Widget _instrumentsGridView() {
-    List<String> instruments = [
-      'assets/images/drums.jpg',
-      'assets/images/flute.jpg',
-      'assets/images/guitar.jpg',
-      'assets/images/keyboard.jpg',
-      'assets/images/violin.jpg',
+    List<Map<String, String>> instruments = [
+      {'name': 'Drums', 'image': 'assets/images/drums.jpg'},
+      {'name': 'flute', 'image': 'assets/images/flute.jpg'},
+      {'name': 'guitar', 'image': 'assets/images/guitar.jpg'},
+      {'name': 'piano', 'image': 'assets/images/keyboard.jpg'},
+      {'name': 'violin', 'image': 'assets/images/violin.jpg'},
+
+      // Add more instruments as needed
     ];
 
     return Expanded(
       child: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
-          //mainAxisSpacing: 1,
-          //crossAxisSpacing: 1,
+          mainAxisSpacing: 1,
+          crossAxisSpacing: 1,
         ),
         itemCount: instruments.length,
         itemBuilder: (BuildContext context, int index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                instruments[index],
-                height: _deviceHeight! * 0.5,
-                width: _deviceWidth! * 2,
-              ),
-            ],
-          );
+          return _instrument1Container();
         },
       ),
     );
+  }
+
+  Widget _instrument1Container() {
+    return Stack(
+      children: [
+        _drumImage(),
+        _instrumentName(),
+        _instrumentDetails(),
+      ],
+    );
+  }
+
+  Widget _drumImage() {
+    return SizedBox(
+      height: _deviceHeight! * 0.45,
+      width: _deviceWidth! * 0.45,
+      child: Image.asset(
+        'assets/images/drums.jpg',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _instrumentName() {
+    return Text(
+      'Drums',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+  }
+
+  Widget _instrumentDetails() {
+    return Text('Instrument Details');
   }
 }
